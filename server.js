0c5085a7bb8865e31d0a4326a245200932f77a7b
Christoffer Palm <christoffer.palm@oddhill.se>
2015-04-17 09:13:00 +0200

    Minor changes to the server.js file

--- server.js
+++ server.js
@@ -8,21 +8,21 @@ global.__server = process.cwd() + '/server/';
 global.__modules = process.cwd() + '/server/modules/';
 
 /**
- * Load server configuration.
- */
-var config = require(__server + 'config');
-
-/**
  * Run the init method to bootstrap the server application. This initializes
  * everything like express, mongoose, passport etc.
  */
 var app = require(__server + 'start/bootstrap')();
 
 /**
+ * Load server configuration, this is handled by the config module.
+ */
+var config = require('config');
+
+/**
  * Start the server.
  */
 app.listen(config.app.port, function() {
-  console.log('Odd mean server started on port ' + config.app.port + ' (Running in ' + process.env.NODE_ENV + ' mode)');
+  console.log(config.app.name + ' server started on port ' + config.app.port + ' (Running in ' + process.env.NODE_ENV + ' mode)');
 });
 
 /**
