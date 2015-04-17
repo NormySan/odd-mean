88616f59c380e3a5986ba986be8a4161c81dabfd
Christoffer Palm <christoffer.palm@oddhill.se>
2015-04-17 09:12:44 +0200

    Set the environment when bootstrapping the server

--- server/start/bootstrap.js
+++ server/start/bootstrap.js
@@ -1,6 +1,14 @@
 'use strict';
 
 /**
+ * Sets the environment variable to development if undefined, otherwise it
+ * will keep the currently configured environment.
+ */
+var setEnvironment = function() {
+  process.env.NODE_ENV = (typeof process.env.NODE_ENV !== 'undefined') ? process.env.NODE_ENV : 'development';
+};
+
+/**
  * Bootstrap the server.
  *
  * @return {object} The configured express app.
